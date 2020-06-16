8dba574190fb88eac98f9966c057e214ac96cd33
Prageeth Silva <prageethsilva@gmail.com>
2020-06-17 00:12:24 +0200

    remove old files

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import { BrowserTabTracker } from './browser-tab-tracker';
-
-const browserTabTrackerSingleton = new BrowserTabTracker();
-
-export {
-	browserTabTrackerSingleton as default
-};
--- test/utils/sleep.ts
+++
@@ -1,5 +0,0 @@
-export const sleep = (duration: number): Promise<void> => {
-    return new Promise(resolve => {
-        setTimeout(resolve, duration);
-    });
-};
